d7f32bae4f454a9938c22ab213fb7f58607f4c42
Illia <1gtagat@gmail.com>
2022-07-30 14:06:10 +0300

    finish BookingController

--- src/main/java/com/illiapinchuk/carrentalsystem/controller/BookingController.java
+++ src/main/java/com/illiapinchuk/carrentalsystem/controller/BookingController.java
@@ -6,6 +6,8 @@ import com.illiapinchuk.carrentalsystem.service.interfaces.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/booking")
 public class BookingController {
@@ -13,12 +15,27 @@ public class BookingController {
     @Autowired
     private BookingService bookingService;
 
+    @GetMapping("/{bookingId}")
+    public Booking getBooking(@PathVariable Long bookingId){
+        return bookingService.getBookingsById(bookingId);
+    }
+
+    @GetMapping
+    public List<Booking> getListOfBookings(){
+        return bookingService.getAllBookings();
+    }
+
+    @GetMapping("/users/{userId}")
+    public List<Booking> getUserBookings(@PathVariable Long userId){
+        return bookingService.getUserBookings(userId);
+    }
+
     @PostMapping("/addBooking")
     public Booking addBooking(@ModelAttribute BookingDto bookingDto){
         return bookingService.addBooking(bookingDto);
     }
 
-    @DeleteMapping("/{bookingId}/deleteBooking")
+    @DeleteMapping("/{bookingId}")
     public void deleteBooking(@PathVariable Long bookingId){
         bookingService.cancelBooking(bookingId);
     }
